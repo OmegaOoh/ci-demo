fc19b1a796af760c6502f9e923111a3cd87bfd3c
Sirapob Phasut <omegaooh@gmail.com>
2024-09-16 15:33:30 +0700

    Add test to cover average and empty list

--- test_statistics.py
+++ test_statistics.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from statistics import variance, stdev
+from statistics import variance, stdev, average
 from math import sqrt
 
 class StatisticsTest(TestCase):
@@ -9,7 +9,7 @@ class StatisticsTest(TestCase):
         self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
         self.assertEqual(2.0, variance([1,2,3,4,5]))
         self.assertEqual(8.0, variance([10,2,8,4,6]))
-    
+
     def test_variance_non_integers(self):
         """variance should work with decimal values"""
         # variance([x,y,z]) == variance([x+d,y+d,z+d]) for any d
@@ -25,8 +25,25 @@ class StatisticsTest(TestCase):
         # variance([0, 0.5, 1, 1.5, 2.0]) is 0.5
         self.assertEqual(sqrt(0.5), stdev([0, 0.5, 1, 1.5, 2]))
 
+    def test_average(self):
+        """average function should work with any numeric values"""
+        # integer value, float results
+        self.assertEqual(1.5, average([1, 2]))
+        # integer value, integer results
+        self.assertEqual(2, average([1, 3]))
+        # float value
+        self.assertEqual(2.5, average([1.5, 3.5]))
+        # mixed value
+        self.assertEqual(2.25, average([2.5, 2]))
+
+    def test_nodata(self):
+        """ test stdev, average, varience with empty list"""
+        # average
+        self.assertRaises(ValueError, average, [])
+        # variance
+        self.assertRaises(ValueError, variance, [])
+
 
 if __name__ == '__main__':
     import unittest
     unittest.main(verbosity=1)
-
